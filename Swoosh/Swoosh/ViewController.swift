4f0db98812f527c74c8e7802edf51eaadacb6a71
Gleb Zavizenov <gleb.zavizenov@gmail.com>
2019-05-27 00:14:31 -0400

    Added 2 forward segues and 1 back segue

--- Swoosh/Swoosh/ViewController.swift
+++ Swoosh/Swoosh/ViewController.swift
@@ -18,6 +18,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func unwindFromLeagueVC(unwindSegue : UIStoryboardSegue) {
+        
+    }
 
 
 }
